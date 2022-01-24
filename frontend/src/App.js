fc08ab227a19cc5e58d19f016d051142e6c9d06a
jackiestchen <jackiestchen@gmail.com>
2022-01-23 21:46:30 -0700

    Add product rout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
       <main className="py-3">
         <Container>
           <Routes>
-            <Route path="/" element={<HomeScreen />} />
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+
           </Routes>
         </Container>
       </main>
