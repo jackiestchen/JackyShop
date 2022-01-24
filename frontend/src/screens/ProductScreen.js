fd0242272766897d92d22f11b2867f779ddefb23
jackiestchen <jackiestchen@gmail.com>
2022-01-23 22:18:15 -0700

    Add ProductScreen component and link to URL params

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
 
 function ProductScreen() {
+  const { id } = useParams();
+  const product = products.find((p) => p._id === id );
+  
   return (
     <div>
-      Product
+      {product.name}
     </div>
   )
 }
