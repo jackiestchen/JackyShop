5efdd3152883d5bd93c162a85074f25f8d70c6fa
jackiestchen <jackiestchen@gmail.com>
2022-01-23 22:18:42 -0700

    Add product route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,6 @@ function App() {
           <Routes>
             <Route path="/" element={<HomeScreen />} exact />
             <Route path="/product/:id" element={<ProductScreen />} />
-
           </Routes>
         </Container>
       </main>
