c9fda967a47ae4e2fc532703342aa77ef742bc35
jackiestchen <jackiestchen@gmail.com>
2022-01-27 19:30:17 -0700

    Fix error message

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -13,6 +13,12 @@ export const listProducts = () => async (dispatch) => {
 
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
-    dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
   }
 };
