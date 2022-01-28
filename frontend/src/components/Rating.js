dffc49634684a204116fbcd39308d9fa22ed7faa
jackiestchen <jackiestchen@gmail.com>
2022-01-27 20:15:59 -0700

    Add classname

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Rating = ({ value, text, color }) => {
   return (
-    <div>
+    <div className="rating">
       <span>
         <i
           style={{ color }}
