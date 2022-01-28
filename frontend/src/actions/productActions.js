2cc33745bca74a85de49c14fba2fb1904bbf4ccf
jackiestchen <jackiestchen@gmail.com>
2022-01-27 19:29:16 -0700

    Add error handling

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -12,5 +12,7 @@ export const listProducts = () => async (dispatch) => {
     const { data } = await axios.get("/api/products");
 
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
-  } catch (error) {}
+  } catch (error) {
+    dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
+  }
 };
