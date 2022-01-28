4f8d295a980951910e8e718feab23b243f39a7f3
jackiestchen <jackiestchen@gmail.com>
2022-01-27 19:41:15 -0700

    Add product reducer

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,20 @@
 import React, { useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
+import { productListReducer } from "../reducers/productReducers";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
