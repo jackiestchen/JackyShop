02b4f8a27c978651e9e7471be827284f77769864
jackiestchen <jackiestchen@gmail.com>
2022-01-29 12:42:18 -0700

    Initialize component

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Row, Col, ListGroup, Image, Form, Button, Card } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { addToCart } from "../actions/cartActions";
+import Message from "../components/Message";
+
 
 const CartScreen = () => {
-  return <div></div>;
+
+  return <div>Cart</div>;
 };
 
 export default CartScreen;
