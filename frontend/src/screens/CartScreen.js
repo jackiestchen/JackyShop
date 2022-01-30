4265d270c1387ea488b1fb8db5ba1c194e2a7142
jackiestchen <jackiestchen@gmail.com>
2022-01-29 22:47:29 -0700

    Implement cartScreen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,12 +1,43 @@
 import React, { useEffect } from "react";
-import { Row, Col, ListGroup, Image, Form, Button, Card } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import {
+  Link,
+  useParams,
+  useSearchParams,
+  useNavigate,
+} from "react-router-dom";
 import { addToCart } from "../actions/cartActions";
 import Message from "../components/Message";
 
-
 const CartScreen = () => {
+  const { id } = useParams();
+
+  const [searchParams] = useSearchParams();
+
+  const qty = Number(searchParams.get("qty"));
+
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const cart = useSelector(state => state.cart);
+  const { cartItems } = cart;
+
+
+  useEffect(() => {
+    if (id) {
+      dispatch(addToCart(id, qty));
+    }
+  }, [dispatch, id, qty]);
 
   return <div>Cart</div>;
 };
