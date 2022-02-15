1b3182f122f91683dcd76f7f9a81cb0093f6a772
jackiestchen <jackiestchen@gmail.com>
2022-02-14 22:54:26 -0700

    Add remove user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,7 @@ import {
   getUserProfile,
   updateUserProfile,
   getUsers,
+  deleteUser,
 } from "../controllers/userController.js";
 import { admin, protect } from "../middleware/authMiddleware.js";
 
@@ -14,5 +15,6 @@ router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/login", authUser);
 router.route("/profile").get(protect, getUserProfile);
 router.route("/profile").put(protect, updateUserProfile);
+router.route("/:id").delete(protect, admin, deleteUser);
 
 export default router;
