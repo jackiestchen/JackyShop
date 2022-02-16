697dc5aa3cc8c75a8abddcb173b43a48795a8f48
jackiestchen <jackiestchen@gmail.com>
2022-02-15 22:08:45 -0700

    Add update user route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,8 @@ import {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 } from "../controllers/userController.js";
 import { admin, protect } from "../middleware/authMiddleware.js";
 
@@ -15,6 +17,10 @@ router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/login", authUser);
 router.route("/profile").get(protect, getUserProfile);
 router.route("/profile").put(protect, updateUserProfile);
-router.route("/:id").delete(protect, admin, deleteUser);
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
