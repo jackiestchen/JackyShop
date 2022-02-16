f42ea4020a86c0fd06fe75eb5febb33d6b280f60
jackiestchen <jackiestchen@gmail.com>
2022-02-15 19:08:56 -0700

    Add get user by id route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -133,5 +133,19 @@ const deleteUser = expressAsyncHandler(async (req, res) => {
 
 });
 
+// @desc    Get user by id
+// @route   GET /api/users/:id
+// @access  Private/admin
+const getUserById = expressAsyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password");
+  
+  if(user) {
+    res.json(user)
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser };
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserById };
