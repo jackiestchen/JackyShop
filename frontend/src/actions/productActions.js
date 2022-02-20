f0daff0770a1d682ec6a56ac4ff738dc369b0275
jackiestchen <jackiestchen@gmail.com>
2022-02-19 17:44:54 -0700

    Add create product action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -39,6 +39,7 @@ export const listProductDetails = (id) => async (dispatch) => {
     const { data } = await axios.get(`/api/products/${id}`);
 
     dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+    
   } catch (error) {
     dispatch({
       type: PRODUCT_DETAILS_FAIL,
