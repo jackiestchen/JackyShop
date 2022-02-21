270482ad623bc080e5d99302941b5e46efd36c26
jackiestchen <jackiestchen@gmail.com>
2022-02-20 23:48:34 -0700

    Add deliver order to route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,6 +4,7 @@ import {
   getAllOrders,
   getMyOrders,
   getOrderById,
+  updateOrderToDelivered,
   updateOrderToPaid,
 } from "../controllers/orderController.js";
 import { admin, protect } from "../middleware/authMiddleware.js";
@@ -17,5 +18,6 @@ router
 router.route("/myorders").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
+router.route("/:id/deliver").put(protect, admin, updateOrderToDelivered);
 
 export default router;
