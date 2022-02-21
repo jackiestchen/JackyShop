3c1c355831a4b89dba25d94846674b9c5ce42b52
jackiestchen <jackiestchen@gmail.com>
2022-02-21 16:02:02 -0700

    Formatting

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -9,23 +9,23 @@ const SearchBox = ({ navigate }) => {
     if (keyword.trim()) {
       navigate(`/search/${keyword}`);
     } else {
-      navigate('/');
+      navigate("/");
     }
   };
 
   return (
     <Form onSubmit={submitHandler} inline>
       <InputGroup>
-      <Form.Control
-        type="text"
-        name="q"
-        onChange={(e) => setKeyword(e.target.value)}
-        placeholder="Search Products..."
-        className="mr-sm-2 ml-sm-5"
-      ></Form.Control>
-      <Button type="submit" variant="outline-success" className="p-2">
-        Search
-      </Button>
+        <Form.Control
+          type="text"
+          name="q"
+          onChange={(e) => setKeyword(e.target.value)}
+          placeholder="Search Products..."
+          className="mr-sm-2 ml-sm-5"
+        ></Form.Control>
+        <Button type="submit" variant="outline-success" className="p-2">
+          Search
+        </Button>
       </InputGroup>
     </Form>
   );
