acfbb03ea4c4e6eec8fd755d611f17081d6e3f91
jackiestchen <jackiestchen@gmail.com>
2022-02-27 16:06:54 -0700

    Add top product routes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -144,6 +144,13 @@ const createProductReview = expressAsyncHandler(async (req, res) => {
     }
   }
 });
+// @desc    GET top rated products
+// @route   GET /api/products/top
+// @access  Public/
+const getTopProducts = expressAsyncHandler(async (req, res) => {
+  const { products } = await Product.find({}).sort({ rating: -1 }).limit(3);
+  res.json(products);
+});
 
 export {
   getProducts,
@@ -152,4 +159,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 };
