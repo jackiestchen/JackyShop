2baec5b968b68f72267522b7529dd8060572debb
jackiestchen <jackiestchen@gmail.com>
2022-02-28 21:41:16 -0700

    Fix route error

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,17 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
@@ -33,17 +44,6 @@ if (process.env.NODE_ENV === "production") {
     res.send("API is running...");
   });
 }
-
-app.use(express.json());
-
-app.use("/api/products", productRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/upload", uploadRoutes);
-
-app.get("/api/config/paypal", (req, res) =>
-  res.send(process.env.PAYPAL_CLIENT_ID)
-);
 
 app.use(notFound);
 
