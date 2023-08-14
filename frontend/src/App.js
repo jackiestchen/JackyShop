586df7c0f10389bf62ac78ac40be4ff27edc6453
Jacky Tchen <jacky@finnovate.io>
2023-08-14 07:46:49 -0600

    Initialize flagsmith in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,38 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
-import CartScreen from "./screens/CartScreen";
-import LoginScreen from "./screens/LoginScreen";
-import RegisterScreen from "./screens/RegisterScreen";
-import ProfileScreen from "./screens/ProfileScreen";
-import ShippingScreen from "./screens/ShippingScreen";
-import PaymentScreen from "./screens/PaymentScreen";
-import PlaceorderScreen from "./screens/PlaceorderScreen";
-import OrderScreen from "./screens/OrderScreen";
-import UserListScreen from "./screens/UserListScreen";
-import UserEditScreen from "./screens/UserEditScreen";
-import ProductListScreen from "./screens/ProductListScreen";
-import ProductEditScreen from "./screens/ProductEditScreen";
-import OrderListScreen from "./screens/OrderListScreen";
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceorderScreen from './screens/PlaceorderScreen';
+import OrderScreen from './screens/OrderScreen';
+import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
+import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
+import OrderListScreen from './screens/OrderListScreen';
+import { useSelector } from 'react-redux';
+import { useFlagsmith } from 'flagsmith/react';
+import NewFeatureScreen from './screens/NewFeatureScreen';
 
 function App() {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  const flagsmith = useFlagsmith();
+
+  useEffect(() => {
+    const identify = () => {
+      flagsmith.identify(userInfo._id);
+    };
+    identify();
+  }, [userInfo._id]);
   return (
     <Router>
       <Header />
@@ -29,29 +42,39 @@ function App() {
             <Route path="/login" element={<LoginScreen />} />
             <Route path="/register" element={<RegisterScreen />} />
             <Route path="/shipping" element={<ShippingScreen />} />
-            
+            <Route path='/newfeature' element={<NewFeatureScreen />} />
+
             <Route path="/placeorder" element={<PlaceorderScreen />} />
             <Route path="/orders/:id" element={<OrderScreen />} />
             <Route path="/payment" element={<PaymentScreen />} />
             <Route path="/profile" element={<ProfileScreen />} />
 
             <Route path="/products/:id" element={<ProductScreen />} />
-            
+
             <Route path="/cart" element={<CartScreen />} />
             <Route path="/cart/:id" element={<CartScreen />} />
             <Route path="/admin/userlist" element={<UserListScreen />} />
-            <Route path="/admin/productlist/:pageNumber" element={<ProductListScreen />} exact/>
+            <Route
+              path="/admin/productlist/:pageNumber"
+              element={<ProductListScreen />}
+              exact
+            />
             <Route path="/admin/productlist" element={<ProductListScreen />} />
             <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
-            <Route path="/admin/orderlist" element={<OrderListScreen />} />          
-            <Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />          
+            <Route path="/admin/orderlist" element={<OrderListScreen />} />
+            <Route
+              path="/admin/product/:id/edit"
+              element={<ProductEditScreen />}
+            />
 
-            <Route path="/search/:keyword" element={<HomeScreen />}/>
-            <Route path="/page/:pageNumber" element={<HomeScreen />} exact/>
-            <Route path="/search/:keyword/page/:pageNumber" element={<HomeScreen />} exact/>
+            <Route path="/search/:keyword" element={<HomeScreen />} />
+            <Route path="/page/:pageNumber" element={<HomeScreen />} exact />
+            <Route
+              path="/search/:keyword/page/:pageNumber"
+              element={<HomeScreen />}
+              exact
+            />
             <Route path="/" element={<HomeScreen />} exact />
-
-
           </Routes>
         </Container>
       </main>
